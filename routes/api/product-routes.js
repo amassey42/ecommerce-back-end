44a58352f0e90f1f2e78b3b69d51293f8d0b0d61
andrew <ammassey42@gmail.com>
2022-11-03 20:33:01 -0700

    finished product-routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -133,8 +133,21 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const product = await Product.destroy({
+      where:{
+        id: req.params.id
+      }
+    })
+    if (!product){
+      return res.status(400).json({message: 'No product Found!'})
+    }
+    res.status(200).json(product)
+  }catch (err){
+    console.log(err)
+  }
 });
 
 module.exports = router;
